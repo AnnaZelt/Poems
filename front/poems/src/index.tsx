c40791a8fbb888eb272bebd61ef187343b8666a8
Anton Laptop <magicdude4@gmail.com>
2024-03-15 22:42:06 +0200

    maybe tomorrow

--- front/poems/src/index.tsx
+++ front/poems/src/index.tsx
@@ -7,8 +7,10 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
+  </React.StrictMode>
 );
 
