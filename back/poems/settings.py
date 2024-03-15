82c5e17c1074cf26ebf5f08d9f846d8ea7160b1c
Anton Laptop <magicdude4@gmail.com>
2024-03-16 00:25:23 +0200

    late night 3

--- back/poems/settings.py
+++ back/poems/settings.py
@@ -190,4 +190,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ORIGINS = ['https://aipoems.netlify.app/']
+# CORS_ALLOW_ORIGINS = ['https://aipoems.netlify.app/']
