d41425cfe71b6b47aefe95c6576445e2b11157a6
Anton Laptop <magicdude4@gmail.com>
2024-03-16 16:31:49 +0200

    cors 2

--- back/poems/settings.py
+++ back/poems/settings.py
@@ -193,6 +193,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ORIGIN_WHITELIST = [
+    'https://aipoems.netlify.app',
+]
+
 CORS_ALLOWED_ORIGINS = ['https://aipoems.netlify.app']
 
 CORS_ALLOW_METHODS = (
