bc4ad066343ab3658607f5af4be5b7db8f6fbd17
Anton Laptop <magicdude4@gmail.com>
2024-04-15 20:33:11 +0300

    hmm 6

--- back/poems/settings.py
+++ back/poems/settings.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure--0h(bt3!td$w+$-%yfh0r!1c=fkiqx90v3o%)hk8$jqx-^+e&7'
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
